ec0a39170f3793ee3ae60090379ede969a9465f7
nosoop <nosoop@users.noreply.github.com>
2023-05-04 03:40:02 -0700

    Add plugin compilation stage to AMBuild

--- configure.py
+++ configure.py
@@ -12,6 +12,10 @@ builder.options.add_option('--mms-path', type=str, dest='mms_path', default=None
                        help='Path to Metamod:Source')
 builder.options.add_option('--sm-path', type=str, dest='sm_path', default=None,
                        help='Path to SourceMod')
+builder.options.add_option('--spcomp-path', type=str, dest='spcomp_path', default=None,
+                       help='Path to directory containing spcomp')
+builder.options.add_option('--no-plugins', action='store_true',
+                       help='Do not compile plugins')
 builder.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
                        help='Enable debugging symbols')
 builder.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
